c450a1ba3b76edf94c211761854afb84c32fee1e
Jay Raval <jay.ambrish.raval@gmail.com>
2023-12-07 17:19:51 -0500

    update rewrite

--- middleware.ts
+++ middleware.ts
@@ -13,6 +13,8 @@ export function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
 
   console.log(requestHeaders);
+  
+  requestHeaders.set("Cache-Control", "no-store");
 
   // Extract country
   const country = request.headers.get("cloudfront-viewer-country");
@@ -23,27 +25,12 @@ export function middleware(request: NextRequest) {
 
   // Specify the correct route based on the requests location
   if (country === RESTRICTED_COUNTRY) {
-    response.headers.set("Cache-Control", "no-store");
-    console.log(response.headers);
-    return NextResponse.rewrite(new URL("/restrict", request.url));
+    return NextResponse.rewrite(new URL("/restrict", request.url), {
+        request: {
+            headers: 
+        }
+    });
   }
+  
+  console.log(response.headers);
 }
-
-// middleware.ts
-// import { NextRequest, NextResponse } from "next/server";
-// const RESTRICTED_COUNTRIES = ["IN", "US"];
-
-// export async function middleware(request: NextRequest) {
-//   const requestHeaders = new Headers(request.headers);
-
-//   console.log(requestHeaders);
-
-//   const country = request.headers.get("cloudfront-viewer-country") || "";
-
-//   console.log(country);
-
-//   if (RESTRICTED_COUNTRIES.includes(country)) {
-//     requestHeaders.set("Cache-Control", "no-cache");
-//     return NextResponse.rewrite(new URL("/restrict", request.url));
-//   }
-// }
